b9440c01ba40b67807be4631da6797cd8860ca00
Giovanni Buffa <tech@giovannibuffa.it>
2023-07-10 14:34:16 +0200

    Change definition on routes callback for Statamic 4 and Laravel 9

--- routes/cp.php
+++ routes/cp.php
@@ -2,8 +2,8 @@
 
 Route::view('metatags', 'statamic-metatags::index')->name('metatags.index');
 
-Route::get('metatags/settings', 'SettingsController@edit')->name('metatags.settings');
-Route::post('metatags/settings', 'SettingsController@update')->name('metatags.settings.update');
+Route::get('metatags/settings', [\Gioppy\StatamicMetatags\Http\Controllers\SettingsController::class, 'edit'])->name('metatags.settings');
+Route::post('metatags/settings', [\Gioppy\StatamicMetatags\Http\Controllers\SettingsController::class, 'update'])->name('metatags.settings.update');
 
-Route::get('metatags/defaults', 'DefaultsController@edit')->name('metatags.defaults');
-Route::post('metatags/defaults', 'DefaultsController@update')->name('metatags.defaults.update');
+Route::get('metatags/defaults', [\Gioppy\StatamicMetatags\Http\Controllers\DefaultsController::class, 'edit'])->name('metatags.defaults');
+Route::post('metatags/defaults', [\Gioppy\StatamicMetatags\Http\Controllers\DefaultsController::class, 'update'])->name('metatags.defaults.update');
