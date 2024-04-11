deccf0a3e24bb42b5cea47062624930f930b9afe
Giovanni Buffa <giova.buffa@gmail.com>
2024-04-11 12:28:21 +0200

    Add permissions (#14)
    * Reformat code and add permission on metatags settings
    
    * Fix incorrect namespace rename
    
    ---------
    
    Co-authored-by: Giovanni Buffa <tech@giovannibuffa.it>

--- src/Tags/MetatagsTags.php
+++ src/Tags/MetatagsTags.php
@@ -4,65 +4,67 @@
 namespace Gioppy\StatamicMetatags\Tags;
 
 
-use Gioppy\StatamicMetatags\DefaultMetatags;
-use Gioppy\StatamicMetatags\Settings;
+use Gioppy\StatamicMetatags\Services\DefaultMetatags;
+use Gioppy\StatamicMetatags\Services\Settings;
 use Illuminate\Support\Str;
 use Statamic\Fields\Value;
 use Statamic\Fieldtypes\Text;
 use Statamic\Support\Arr;
 use Statamic\Tags\Tags;
 
-class MetatagsTags extends Tags {
+class MetatagsTags extends Tags
+{
 
-  protected static $handle = 'metatags';
+    protected static $handle = 'metatags';
 
-  public function index() {
-    $settings = Settings::make();
+    public function index()
+    {
+        $settings = Settings::make();
 
-    $settingsMeta = $settings->onlyMeta();
-    $settingsDefault = $settings->excludedMeta();
+        $settingsMeta = $settings->onlyMeta();
+        $settingsDefault = $settings->excludedMeta();
 
-    $defaultValues = Arr::removeNullValues(DefaultMetatags::make()->augmented());
+        $defaultValues = Arr::removeNullValues(DefaultMetatags::make()->augmented());
 
-    // Get page metatags and remove null values
-    $pageFields = $this->context->filter(function ($item, $key) use ($settingsMeta) {
-      return Str::startsWith($key, $settingsMeta);
-    })
-      ->filter()
-      ->filter(function ($item) {
-        if (is_array($item) && array_key_exists('value', $item) && $item['value'] === null) {
-          return false;
+        // Get page metatags and remove null values
+        $pageFields = $this->context->filter(function ($item, $key) use ($settingsMeta) {
+            return Str::startsWith($key, $settingsMeta);
+        })
+            ->filter()
+            ->filter(function ($item) {
+                if (is_array($item) && array_key_exists('value', $item) && $item['value'] === null) {
+                    return false;
+                }
+
+                return true;
+            })
+            ->all();
+
+        // Merge default values with page metatags
+        $fields = collect($defaultValues)
+            ->filter()
+            ->merge($pageFields);
+
+        // Check if fields have title, otherwise load default title
+        if (array_key_exists('basic_title', $fields->all()) && !is_string($fields->get('basic_title')->value())) {
+            $defaultTitle = $this->context->only('title')->all();
+            $fields->prepend(new Value($defaultTitle['title'] ?? ' '), 'basic_title');
         }
 
-        return true;
-      })
-      ->all();
+        // If there is no basic_title, load page title
+        if (!array_key_exists('basic_title', $fields->all())) {
+            $defaultTitle = $this->context->only('title')->all();
+            $fields->prepend(new Value($defaultTitle['title'] ?? ' ', 'basic_title', new Text()), 'basic_title');
+        }
 
-    // Merge default values with page metatags
-    $fields = collect($defaultValues)
-      ->filter()
-      ->merge($pageFields);
-
-    // Check if fields have title, otherwise load default title
-    if (array_key_exists('basic_title', $fields->all()) && !is_string($fields->get('basic_title')->value())) {
-      $defaultTitle = $this->context->only('title')->all();
-      $fields->prepend(new Value($defaultTitle['title'] ?? ' '), 'basic_title');
+        return view('statamic-metatags::metatags', [
+            'fields' => $fields->all(),
+            'settings' => $settingsMeta,
+            'extras' => [
+                'site_name' => $settingsDefault['site_name'],
+                'site_name_separator' => $settingsDefault['site_name_separator'],
+                'permalink' => $this->context->get('permalink'),
+            ],
+        ]);
     }
-
-    // If there is no basic_title, load page title
-    if (!array_key_exists('basic_title', $fields->all())) {
-      $defaultTitle = $this->context->only('title')->all();
-      $fields->prepend(new Value($defaultTitle['title'] ?? ' ', 'basic_title', new Text()), 'basic_title');
-    }
-
-    return view('statamic-metatags::metatags', [
-      'fields' => $fields->all(),
-      'settings' => $settingsMeta,
-      'extras' => [
-        'site_name' => $settingsDefault['site_name'],
-        'site_name_separator' => $settingsDefault['site_name_separator'],
-        'permalink' => $this->context->get('permalink'),
-      ],
-    ]);
-  }
 }
