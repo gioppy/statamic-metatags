ed443f00ce949ee78f53faf99e36240e297b9e8a
Giovanni Buffa <tech@giovannibuffa.it>
2024-06-04 15:24:10 +0200

    Fix view for empty default fields

--- resources/views/defaults/edit.blade.php
+++ resources/views/defaults/edit.blade.php
@@ -2,11 +2,16 @@
 @section('title', 'Defaults Meta tags')
 
 @section('content')
-  <publish-form
-    title="{{ $title }}"
-    action="{{ $action }}"
-    :blueprint='@json($blueprint)'
-    :meta='@json($meta)'
-    :values='@json($values)'
-  ></publish-form>
+  @if($blueprint['empty'])
+    <h1>{{ __('Defaults') }}</h1>
+    <p>No default fields found!</p>
+  @else
+    <publish-form
+      title="{{ $title }}"
+      action="{{ $action }}"
+      :blueprint='@json($blueprint)'
+      :meta='@json($meta)'
+      :values='@json($values)'
+    ></publish-form>
+  @endif
 @endsection
